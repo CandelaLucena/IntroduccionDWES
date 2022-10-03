cb94ab3e7608eb4ec9f13c5d87995766d0c85852
CandelaLucena <candelalucenareyes1212@gmail.com>
2022-10-03 13:29:22 +0200

    FORMULARIO TERMINADO

--- formulario.php
+++ formulario.php
@@ -1,14 +1,47 @@
 <?php
+function filtrado($datos){
+    $datos = trim($datos); // Elimina espacios antes y después de los datos
+    $datos = stripslashes($datos); // Elimina backslashes 
+    $datos = htmlspecialchars($datos); // Traduce caracteres especiales en entidades HTML
+    return $datos;
+}
 
+if(isset($_POST["submit"])){
+    $nombre = filtrado($_POST["nombre"]);
+    $educacion = filtrado($_POST["educacion"]);
+    $email = filtrado($_POST["email"]);
+    $avatar = $_FILES["avatar"];
+
+    
+    $nombreArchivo = $_FILES['avatar']['name'];
+    $directorioTemp = $_FILES['avatar']['tmp_name'];
+    $extension = $arrayArchivo['extension'];
+    $nombreCompleto = $directorioSubida.$nombreArchivo.".".$extension;
+    move_uploaded_file($directorioTemp, $nombreCompleto);
+}
+
+if(empty($nombre)){
+    echo("Introduce un nombre");
+}else if(empty($email)){
+    echo("Introduce un email");
+}else{
+
+?>
+    <h2>Mostrar datos enviados</h2>
+    Nombre : <?= $nombre ?? "" ?> <br>
+    Educación : <?= $educacion ?? "" ?> <br>
+    Email : <?= $email ?? "" ?> <br>
+    Avatar : <?= $avatar ?? "" ?> <br>
+<?php
+}
 ?>
 
 
+
 <html>
-
 <body>
-
 <h2>Formulario</h2>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
     Nombre<br>
     <input type="text" name="nombre" maxlength="50"><br><br>
     Email<br>
@@ -21,7 +54,7 @@
         <option value="universidad">Universidad</option>
     </select> <br><br>
     Avatar
-    <br><br>
+    <input type="file" name="avatar"><br><br>
     <input type="submit" name="submit" value="Enviar">
 </form>
 </body>
